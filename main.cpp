28faf3c896ec1ea5f0dfd639d029ea941950c811
Chantal2015 <chantalsaadhajjar@gmail.com>
2024-11-08 15:46:46 +0200

    Push method

--- main.cpp
+++ main.cpp
@@ -6,7 +6,17 @@ private:
   int* p ; // allouer de la mémoire pour un élément
   int top; // contient l’indice du dernier élément ajouté à la pile
   int capacity; // capacite du stack
-
+ void resizeStack(int new_max){
+      int* new_p = new int[new_max];
+    for (int i = 0; i <= top; ++i) {
+        new_p[i] = p[i];
+    }
+    delete[] p;
+    p = new_p;
+    capacity = new_max;
+    cout << "Stack is resized. Maximal Capacity is " << new_max << std::endl;
+}
+ 
 
 public:
   Stack(int capacity, int top = -1):capacity(capacity), top(top),p(new int[capacity]){
@@ -21,7 +31,15 @@ public:
     }
   }
   ~Stack(){delete[] p; p = nullptr;}
-  
+  void push(int value){
+      if (top >= capacity - 1) {
+        resizeStack(capacity * 2);
+    }
+    top++;
+    p[top] = value;
+    std::cout << value << " is pushed to stack" << std::endl;
+}
+
   void print(){
     if (top == -1){
         cout << "Stack empty" << endl;
@@ -42,6 +60,9 @@ public:
 
 int main(){
   Stack st1(1);
+  st1.print();
+  st1.push(8);
+  st1.push(7);
   st1.print();
   int tab [] = {1,6,7,87,8};
   Stack st2(tab, 5);
